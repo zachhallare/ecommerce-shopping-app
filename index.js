186e7809abcea1d5cd831961b83be09ec84435f6
Zach Benedict Hallare <zachhallare@gmail.com>
2025-12-27 22:30:03 +0800

    Connected to the database.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-console.log("Hello");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+mongoose
+    .connect(MONGO_URI)
+    .then(() => console.log("DB Connected Successful"))
+    .catch((err) => {
+        console.log(err);
+    });
+
+app.listen(5000, () => {
+    console.log("Server is running");
+});
 
