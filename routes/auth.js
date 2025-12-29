aa270e5c86c37925b8b707d110da0b91cf190db9
Zach Benedict Hallare <zachhallare@gmail.com>
2025-12-29 23:55:03 +0800

    Added APIs for verification.

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");  // for encryption.
+const jwt = require("jsonwebtoken")
 
 // register.
 router.post("/register", async (req, res) => {
@@ -23,7 +24,36 @@ router.post("/register", async (req, res) => {
 
 // login.
 router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({username: req.body.username});
+        !user && res.status(401).json("Wrong credentials!");
 
+        const hashedPassword = CryptoJS.AES.decrypt(
+            user.password, 
+            process.env.PASS_SEC
+        );
+
+        const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+        originalPassword !== req.body.password && 
+            res.status(401).json("Wrong credentials!");
+            const accessToken = jwt.sign(
+                {
+                    id: user._id, 
+                    isAdmin: user.isAdmin,
+                }, 
+                process.env.JWT_SEC,
+                {
+                    expiresIn: "3d"
+                }
+            );
+
+        const {password, ...others} = user._doc;
+
+        res.status(200).json({...others, accessToken});
+    } catch(err) {
+        res.status(500).json(err)
+    }
 });
 
 
